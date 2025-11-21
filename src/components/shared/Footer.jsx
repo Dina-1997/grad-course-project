be87ddf2dab3280ca5fffcb8feeafc6ef3676629
mhmdsalah <ms.mhmdsalah@gmail.com>
2025-11-21 18:06:43 +0200

    update footer style

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer gjj</div>
-    
-  )
-}
+    <div className="text-base text-center py-3 absolute bottom-0 left-0 w-full bg-gray-900 text-white">
+      Footer
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
